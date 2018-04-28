fd4b4528ef3dc400b5795d9a912d20e83564807a
pvbarredo <Peter Barredo>
2018-04-28 18:11:59 +0800

    update insert

--- src/com/pobreng/pedro/window/Main.java
+++ src/com/pobreng/pedro/window/Main.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -35,6 +36,7 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.FormSpecs;
 import com.jgoodies.forms.layout.RowSpec;
 import com.pobreng.pedro.model.CriticalValue;
+import com.pobreng.pedro.model.Data;
 import com.pobreng.pedro.model.Device;
 import com.pobreng.pedro.model.Property;
 import javax.swing.JSeparator;
@@ -55,13 +57,17 @@ public class Main {
 	private List<Device> devices = new ArrayList<>();
 	private JLabel deviceUsername;
 	private JLabel deviceName;
-	private JTextField textField;
 	private Map<String, JLabel> dynamicPropertyLabel = new HashMap<String, JLabel>();
-	private Map<String, JTextField> dynamicPropertyTextfield = new HashMap<String, JTextField>();
+	private Map<String, JTextField> dynamicDataTextField = new HashMap<String, JTextField>();
 	
 	JPanel panel_1 ;
 	private JButton btnSubmit;
 	private JScrollPane scrollPane;
+	private JButton btnClearAllDevice;
+	private JButton btnSeedData;
+	private static String APP_URL = "http://localhost:8000";
+	
+	Device selectedDevice;
 
 	/**
 	 * Launch the application.
@@ -106,7 +112,8 @@ public class Main {
 		 	public void mouseClicked(MouseEvent evt) {
 		 	
 		        if (evt.getClickCount() == 2) {
-		            Device selectedDevice = devices.get(list.locationToIndex(evt.getPoint()));
+		            selectedDevice = devices.get(list.locationToIndex(evt.getPoint()));
+		            btnSubmit.setEnabled(true);
 		            setDataForm(selectedDevice);
 		        }
 		 	}
@@ -120,46 +127,20 @@ public class Main {
 			private void addPropertyComponent(Device selectedDevice) {
 				removePropertyComponent();
 				int y_axis = 14;
-				int x_axis = 6; 
 				
 				for (Property property : selectedDevice.getType().getProperty()) {
-					JLabel newLabel = new JLabel("Property : " + property.getName());
+					JLabel newLabel = new JLabel("Property : " + property.getName() + "--" + property.getUnit());
 					dynamicPropertyLabel.put(String.valueOf(property.getId()),newLabel);
 					panel_1.add(newLabel, "6,"+ y_axis +", right, default");
+					
+					JTextField newDataTextField = new JTextField();
+					newDataTextField.setColumns(10);
+					dynamicDataTextField.put(String.valueOf(property.getId()),newDataTextField);
+					panel_1.add(newDataTextField, "8,"+ y_axis +", fill, default");
+					
 					y_axis++;
 					
-					for (CriticalValue criticalValue : property.getCritical_value()) {
-						JLabel newLabel2 = new JLabel("--"+ criticalValue.getDescription() + " - Condition: " + criticalValue.getCondition());
-						dynamicPropertyLabel.put(String.valueOf(criticalValue.getId())+ "Critical",newLabel2);
-						panel_1.add(newLabel2, "8,"+ y_axis +", right, default");
-						
-						y_axis++;
-						
-						JLabel newlabel3 = new JLabel("Min Value");
-						dynamicPropertyLabel.put(String.valueOf(criticalValue.getId())+ y_axis+ "MinVal",newlabel3);
-						panel_1.add(newlabel3, "6,"+ y_axis +", right, default");
-						
-						JTextField newCriticalValueTextField = new JTextField();
-						newCriticalValueTextField.setColumns(10);
-						dynamicPropertyTextfield.put(String.valueOf(criticalValue.getId())+ y_axis+"Min",newCriticalValueTextField);
-						panel_1.add(newCriticalValueTextField, "8,"+ y_axis +", fill, default");
-						
-						y_axis++;
-						
-						JLabel newlabel4 = new JLabel("Max Value");
-						dynamicPropertyLabel.put(String.valueOf(criticalValue.getId())+y_axis+ "MinVal",newlabel4);
-						panel_1.add(newlabel4, "6,"+ y_axis +", right, default");
-						
-						JTextField maxTextField = new JTextField();
-						maxTextField.setColumns(10);
-						dynamicPropertyTextfield.put(String.valueOf(criticalValue.getId())+y_axis+ "Max",maxTextField);
-						panel_1.add(maxTextField, "8,"+ y_axis +", fill, default");
-						
-						y_axis++;
-						
-						
-					}
-					y_axis++;
+					
 					
 				}
 
@@ -177,16 +158,16 @@ public class Main {
 					panel_1.repaint(); 
 				}
 				
-				for(Map.Entry<String, JTextField> propertyTextField : dynamicPropertyTextfield.entrySet()) {
+				for(Map.Entry<String, JTextField> propertyTextField : dynamicDataTextField.entrySet()) {
 					JTextField removeTextField = propertyTextField.getValue();
-					dynamicPropertyTextfield.remove(removeTextField);
+					dynamicDataTextField.remove(removeTextField);
 					panel_1.remove(propertyTextField.getValue());
 					panel_1.revalidate();
 					panel_1.repaint(); 
 				}
 				
 				
-				
+				dynamicDataTextField.clear();
 			
 				
 			}
@@ -198,20 +179,23 @@ public class Main {
 				FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
 				ColumnSpec.decode("28px"),},
 			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
 				FormSpecs.LINE_GAP_ROWSPEC,
 				RowSpec.decode("23px"),
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
-		JButton btnGetDevice = new JButton("GET DEVICE");
+		JButton btnGetDevice = new JButton("GET DEVICES");
 		
 		btnGetDevice.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				
 				try {
 
-					URL url = new URL("http://localhost:8000/device");
+					URL url = new URL(APP_URL + "/device");
 					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 					conn.setRequestMethod("GET");
 					conn.setRequestProperty("Accept", "application/json");
@@ -261,11 +245,76 @@ public class Main {
 				
 			}
 		});
-		panel.add(btnGetDevice, "2, 2, 2, 1, left, top");
+		
+		btnSeedData = new JButton("Seed Data");
+		btnSeedData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+
+					URL url = new URL(APP_URL + "/deviceData/seedData");
+					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+					conn.setRequestMethod("GET");
+					conn.setRequestProperty("Accept", "application/json");
+
+					if (conn.getResponseCode() != 200) {
+						throw new RuntimeException("Failed : HTTP error code : "
+								+ conn.getResponseCode());
+					}
+
+					
+
+					conn.disconnect();
+
+				  } catch (MalformedURLException ex) {
+
+					ex.printStackTrace();
+
+				  } catch (IOException ex) {
+
+					ex.printStackTrace();
+
+				  }
+				
+			}
+		});
+		panel.add(btnSeedData, "2, 2, 3, 1");
+		
+		btnClearAllDevice = new JButton("Clear All Datas");
+		btnClearAllDevice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+
+					URL url = new URL(APP_URL + "/deviceData/deleteAll");
+					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+					conn.setRequestMethod("GET");
+					conn.setRequestProperty("Accept", "application/json");
+
+					if (conn.getResponseCode() != 200) {
+						throw new RuntimeException("Failed : HTTP error code : "
+								+ conn.getResponseCode());
+					}
+
+					
+
+					conn.disconnect();
+
+				  } catch (MalformedURLException ex) {
+
+					ex.printStackTrace();
+
+				  } catch (IOException ex) {
+
+					ex.printStackTrace();
+
+				  }
+			}
+		});
+		panel.add(btnClearAllDevice, "2, 4, 3, 1");
+		panel.add(btnGetDevice, "2, 6, 3, 1, left, top");
 		
 		
 		
-		panel.add(list, "2, 4, 2, 1, left, center");
+		panel.add(list, "2, 8, 2, 1, left, center");
 		
 		panel_1 = new JPanel();
 		frmPobrengPedro.getContentPane().add(panel_1, BorderLayout.CENTER);
@@ -322,6 +371,56 @@ public class Main {
 		panel_1.add(deviceUsername, "8, 4");
 		
 		btnSubmit = new JButton("Submit");
+		btnSubmit.setEnabled(false);
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				System.out.println(selectedDevice.getId());
+				System.out.println(latitudeTextField.getText());
+				System.out.println(longitudeTextField.getText());
+		
+				for (Map.Entry<String, JTextField> entry : dynamicDataTextField.entrySet()) {
+					JTextField dataValue  = entry.getValue();
+				    System.out.println("Key = " + entry.getKey() + ", Value = " + dataValue.getText());
+				    
+				    try {
+				    	String urlParameters = "device_id="+selectedDevice.getId()+"&property_id=" + entry.getKey() + "&value=" + dataValue.getText()+"&latitude="+latitudeTextField.getText()+"&longitude="+longitudeTextField.getText();
+				    	URL url = new URL(APP_URL + "/deviceData/addDeviceData?" + urlParameters);
+						HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+						
+						conn.setRequestMethod("GET");
+						conn.setRequestProperty("Accept", "application/json");
+
+						if (conn.getResponseCode() != 200) {
+							throw new RuntimeException("Failed : HTTP error code : "
+									+ conn.getResponseCode());
+						}
+						
+						int responseCode = conn.getResponseCode();
+						System.out.println("\nSending 'POST' request to URL : " + url);
+						System.out.println("Post parameters : " + urlParameters);
+						System.out.println("Response Code : " + responseCode);
+
+						BufferedReader in = new BufferedReader(
+						        new InputStreamReader(conn.getInputStream()));
+						String inputLine;
+						StringBuffer response = new StringBuffer();
+
+						while ((inputLine = in.readLine()) != null) {
+							response.append(inputLine);
+						}
+						in.close();
+						
+						//print result
+						System.out.println(response.toString());
+						
+					} catch (Exception e2) {
+						// TODO: handle exception
+					}
+				}
+				
+			}
+		});
 		panel_1.add(btnSubmit, "10, 4");
 		
 		JLabel lblNewLabel = new JLabel("Device Name");
